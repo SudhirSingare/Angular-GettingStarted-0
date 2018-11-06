9a603cb24e73f23b25b4a6f793174ba483fb9412
Mark Vogt <markevogt@outlook.com>
2018-11-05 20:04:19 -0600

    RE-CODED AppComponent; RE-CODED index.html; RUNS !

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <<h1>{{pageTitle}}</h1>
+      <div>MV First Component</div>
+    </div>
+  `
+
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Mark Vogt Angular App Page Title';
+
+
 }
